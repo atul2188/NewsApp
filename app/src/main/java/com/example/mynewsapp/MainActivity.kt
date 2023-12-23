21ece3f6f4a63fdf1a7cb6242fa88cd811774125
atul2188 <143529911+atul2188@users.noreply.github.com>
2023-12-23 05:50:26 +0530

    Implementing OnBoarding Screen.

--- app/src/main/java/com/example/mynewsapp/MainActivity.kt
+++ app/src/main/java/com/example/mynewsapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mynewsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mynewsapp.presentation.onboarding.OnBoardingScreen
 import com.example.mynewsapp.ui.theme.MyNewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +22,10 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             MyNewsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)){
+
                 }
+                OnBoardingScreen()
             }
         }
     }
